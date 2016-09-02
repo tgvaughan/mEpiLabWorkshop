57eeb87a4d9c2d562dfb7306562fe3df60310983
Tim Vaughan <tgvaughan@gmail.com>
2016-09-02 15:10:31 +1200

    Added more slides.

--- Bayesian_Inference/urn_example.R
+++ Bayesian_Inference/urn_example.R
@@ -9,3 +9,12 @@ plot(Nr, lik, 'b', col='blue', lwd=2,
      xlab = expression(paste('Value of ', N[r])),
      ylab = expression(P(N[r] ~ '|' ~ R,B,R)))
 dev.off()
+
+
+png("poissonian.png", width=600, height=600, pointsize=20)
+n <- seq(0, 10)
+p <- dpois(n, 5)
+plot(n, p, 'h', lwd=2, col='blue',
+     ylab=expression(P(n)))
+points(n, p, lwd=2, col='blue')
+dev.off()
